8b508d11bb3fac0e3208acacbe9e0cf105bfab50
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 13:27:12 +0200

    Update favorites4.py
    Seid Mamuti - week 8

--- part1/starter/favorites4.py
+++ part1/starter/favorites4.py
@@ -21,3 +21,18 @@ with open("favorites.csv", "r") as file:
         # TODO: Use if/elif to increment the correct counter
 
     # TODO: Print the three counts using f-strings
+import csv
+from pathlib import Path
+with open(csv_path, "r", newline="") as file:
+reader = csv.DictReader(file)
+for row in reader:
+favorite = row["language"]
+if favorite == "Python":
+python += 1
+elif favorite == "C":
+c += 1
+elif favorite == "Scratch":
+scratch += 1
+print(f"Python: {python}")
+print(f"C: {c}")
+print(f"Scratch: {scratch}")
