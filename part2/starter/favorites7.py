7d1cb2e63f20e8a552cb22b337fdeb0223dca9b4
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 13:30:31 +0200

    Update favorites7.py
    Seid Mamuti Week - 8 assignment

--- part2/starter/favorites7.py
+++ part2/starter/favorites7.py
@@ -21,3 +21,17 @@ with open("../week1/favorites.csv", "r") as file:
 #   C: 40
 #   Python: 196
 #   Scratch: 28
+import csv
+from pathlib import Path
+counts = {}
+with open(csv_path, "r", newline="") as file:
+reader = csv.DictReader(file)
+for row in reader:
+favorite = row["language"]
+try:
+counts[favorite] += 1
+except KeyError:
+counts[favorite] = 1
+for favorite in sorted(counts):
+print(f"{favorite}: {counts[favorite]}")
+
