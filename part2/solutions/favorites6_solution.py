4ce11fb19084366490305737ddbf2bc78b2f5a13
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 13:29:23 +0200

    Update favorites6_solution.py
    Seid Mamuti - Week 8 assignment

--- part2/solutions/favorites6_solution.py
+++ part2/solutions/favorites6_solution.py
@@ -13,3 +13,16 @@ with open("../week1/favorites.csv", "r") as file:
 
 for favorite in counts:
     print(f"{favorite}: {counts[favorite]}")
+import csv
+from pathlib import Path
+counts = {}
+with open(csv_path, "r", newline="") as file:
+reader = csv.DictReader(file)
+for row in reader:
+favorite = row["language"]
+try:
+counts[favorite] += 1
+except KeyError:
+counts[favorite] = 1
+for favorite in counts:
+print(f"{favorite}: {counts[favorite]}")
